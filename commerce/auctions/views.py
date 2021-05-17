3823bf976316ab73042a26b3791f2fa88ed74062
zaid butt <xaid.butt.10@gmail.com>
2021-05-17 19:09:46 +0500

    stripe

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -27,10 +27,37 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.db import IntegrityError
 from django.utils.decorators import method_decorator
+from rest_framework.decorators import api_view
 from rest_framework.authtoken.views import ObtainAuthToken
 import json
+import stripe
+
+stripe.api_key = "sk_test_51Is5oDBokRZBbAhwZNSMTxLLcEKtKjxDWIVh1aWhfQPywS3I8qO07rUxzYncAB9cnKKq25qJuwVJChjlVuGvlOHi00gDpNjBKB"
 
 
+@api_view(['POST'])
+def test_payment(request):
+    test_payment_intent = stripe.PaymentIntent.create(
+    amount=1000, currency='pln', 
+    payment_method_types=['card'],
+    receipt_email='test@example.com')
+    return Response(status=status.HTTP_200_OK, data=test_payment_intent)
+
+def save_stripe_info(request):
+    print(request.POST)
+    data = request.POST
+    email = data['email']
+    payment_method_id = data['payment_method_id']
+    
+    # creating customer
+    customer = stripe.Customer.create(
+      email=email, payment_method=payment_method_id)
+     
+    return Response(status=status.HTTP_200_OK, 
+      data={
+        'message': 'Success', 
+        'data': {'customer_id': customer.id}   
+      })                                     
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -97,7 +124,7 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     #authentication_classes = (TokenAuthentication,)
-    permission_classes = [IsAuthenticated]
+    #permission_classes = [IsAuthenticated]
     
 
 
